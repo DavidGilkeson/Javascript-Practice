788cbd43e68c65a5cba451f396d32835a64b4fc8
David Gilkeson <davidgilkeson8@gmail.com>
2020-03-25 20:48:28 +0000

    Updated Higher order functions

---
+++ Higher order functions/main.js
@@ -0,0 +1,172 @@
+const companies = [
+  {
+    name: "Company One",
+    category: "Finance",
+    start: 1981,
+    end: 2003
+  },
+  {
+    name: "Company Two",
+    category: "Retail",
+    start: 1992,
+    end: 2008
+  },
+  {
+    name: "Company Three",
+    category: "Auto",
+    start: 1999,
+    end: 2007
+  },
+  {
+    name: "Company Four",
+    category: "Retail",
+    start: 1989,
+    end: 2010
+  },
+  {
+    name: "Company Five",
+    category: "Technology",
+    start: 2009,
+    end: 2014
+  },
+  {
+    name: "Company Six",
+    category: "Finance",
+    start: 1987,
+    end: 2010
+  },
+  {
+    name: "Company Seven",
+    category: "Auto",
+    start: 1986,
+    end: 1996
+  },
+  {
+    name: "Company Eight",
+    category: "Technology",
+    start: 2011,
+    end: 2016
+  },
+  {
+    name: "Company Nine",
+    category: "Retail",
+    start: 1981,
+    end: 1989
+  }
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// for (let i = 0; i < companies.length; i++) {
+
+//     console.log(companies[i]);
+// }
+
+//ForEach
+
+// companies.forEach(function(company) {
+//   console.log(company.name);
+// });
+
+//filter
+
+// let canDrink = [];
+
+// ages.forEach(function (age) {
+
+//     if (age >= 21) {
+//         canDrink.push(age);
+//     }
+// });
+
+// const canDrink = ages.filter(function (age) {
+
+//     if (age >= 21) {
+//         return true;
+//     }
+// });
+
+// const canDrink = ages.filter(age => age >= 21);
+
+// console.log(canDrink);
+
+// Filter retail companies
+
+// const retailCompanies = companies.filter(company => company.category === "Retail");
+
+// console.log(retailCompanies);
+
+// Get 1980's companies
+
+// const eightiesCompanies = companies.filter(company => company.start >= 1980 && company.start < 1990);
+
+// console.log(eightiesCompanies);
+
+// Get companies that lasted 10 years or more
+
+// const tenYears = companies.filter(company => (company.end - company.start >= 10));
+
+// console.log(tenYears);
+
+// Create an array of company names
+
+// const companyNames = companies.map(function(company) {
+//   return company.name;
+// });
+
+// const testMap = companies.map(function(company) {
+//   // ES template strings.. Template for a variable
+//   return `${company.name} [ ${company.name} - ${company.end}]`;
+// });
+
+// const companyNames = companies.map(company => company.name);
+
+// console.log(companyNames);
+// console.log(testMap);
+
+// const agesSquare = ages.map(age => Math.sqrt(age)).map(age => age * 2);
+
+// console.log(agesSquare);
+
+// Sort
+// Sort companies by start year
+// const sortedCompanies = companies.sort(function(c1, c2) {
+//   if (c1.start > c2.start) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+// const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
+
+// console.log(sortedCompanies);
+
+// const sortAges = ages.sort((a, b) => a - b);
+
+// console.log(sortAges);
+
+//Reduce
+
+// const ageSum = ages.reduce((total, age) => total + age, 0);
+
+// console.log(ageSum);
+
+//Get total years for all companies
+
+// const totalYears = companies.reduce(
+//   (total, company) => total + (company.end - company.start),
+//   0
+// );
+
+// console.log(totalYears);
+
+//Combine Methods
+
+const combined = ages
+  .map(age => age * 2)
+  .filter(age => age >= 40)
+  .sort((a, b) => a - b)
+  .reduce((a, b) => a + b, 0);
+
+console.log(combined);
