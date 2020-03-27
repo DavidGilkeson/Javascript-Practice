55129472ec1518cfdad018197b7828180d4b59d6
David Gilkeson <davidgilkeson8@gmail.com>
2020-03-27 22:53:11 +0000

    OOP added

---
+++ OOP/3_prototypes.js
@@ -0,0 +1,25 @@
+// COnstructor
+
+function Book(title, author, year) {
+
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+
+};
+
+
+
+// getSummary Prototype
+
+Book.prototype.getSummary = function () {
+    return `${book1.title} was written by ${book1.author} in ${book1.year}`;
+}
+
+// Instantiate an object
+
+const book1 = new Book('Book One', 'John Doe', '2013');
+const book2 = new Book('Book Two', 'Jane Doe', '2020');
+
+console.log(book1.getSummary(), book2);
