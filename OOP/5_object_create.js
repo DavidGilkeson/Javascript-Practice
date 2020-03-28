518c4c0ad0cc42d4a8a6e644c01e3c28d429c2d4
David Gilkeson <davidgilkeson8@gmail.com>
2020-03-28 00:57:29 +0000

    Object create added

---
+++ OOP/5_object_create.js
@@ -0,0 +1,36 @@
+// Object Of Protos
+const bookProtos = {
+
+    getSummary: function () {
+
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    },
+
+    getAge: function () {
+
+        const years = new Date().getFullYear() - this.year;
+        return `${book1.title} is ${years} years old`;
+    }
+}
+
+// Create Object
+// const book1 = Object.create(bookProtos);
+// book1.title = 'Book One';
+// book1.author = 'John Doe';
+// book1.year = '1999';
+
+const book1 = Object.create(bookProtos, {
+    title: {
+        value: 'Book One'
+    },
+    author: {
+        value: 'John Doe'
+    },
+    year: {
+        value: '1999'
+    }
+
+});
+
+
+console.log(book1);
